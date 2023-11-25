0143889a8cc48c102b26161081bece33814dc491
JRAUL19 <raulurq132@gmail.com>
2023-11-25 09:51:39 -0600

    Prestamos_PlanDePago

---
+++ WebApiPrestamos/WebApiPrestamos/Entities/PrestamosDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiPrestamos.Entities
+{
+    public class PrestamosDbContext : DbContext
+    {
+        public PrestamosDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        //public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Prestamo> Prestamos { get; set; }
+        public DbSet<PlanDePago> PlanDePagos { get; set; }
+    }
+}
